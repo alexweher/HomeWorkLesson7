0fbb8037f6136ac2abb5e923c3ff0eb22c87a374
alexweher <alexweher@yandex.ru>
2022-06-17 15:24:04 +0300

    HomeWorkLesson7

---
+++ market.homework/src/main/java/ru/gb/market/homework/market/homework/model/Product.java
@@ -0,0 +1,10 @@
+package ru.gb.market.homework.market.homework.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+}
